9a775b9724973d92c2b3639ef64a90601f6c5a80
npejoro <32080367+npejoro@users.noreply.github.com>
2018-02-05 21:04:18 -0800

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,10 @@
 import os
 import csv
 
-file_num = ['1', '2']
+fileext = ['1', '2']
 
-#create file path and save as file
-for filenumber in file_num:
+#for loop to create file path and extract source data
+for filenumber in fileext:
     
     file = os.path.join('raw_data', 'budget_data_' + str(filenumber) + '.csv')
 
@@ -12,7 +12,7 @@ for filenumber in file_num:
     months = []
     revenue = []
 
-#read csv 
+#read csv and calculation to count total months
     with open(file, 'r') as csvfile:
         csvread = csv.reader(csvfile)
     
@@ -22,25 +22,25 @@ for filenumber in file_num:
             months.append(row[0])
             revenue.append(int(row[1]))
 
-    total_months = len(months)
+    totalmonths = len(months)
 
 #create greatest increase, greatest decrease and total revenue
-    greatest_inc = revenue[0]
-    greatest_dec = revenue[0]
-    total_revenue = 0
+    greatinc = revenue[0]
+    greatdec = revenue[0]
+    totalrev = 0
 
-#loop through revenue and calculate greatest increase and greatest decrease
+#for loop to calculate revenue and calculate greatest increase and greatest decrease
     for r in range(len(revenue)):
-        if revenue[r] >= greatest_inc:
-            greatest_inc = revenue[r]
-            great_inc_month = months[r]
-        elif revenue[r] <= greatest_dec:
-            greatest_dec = revenue[r]
-            great_dec_month = months[r]
-        total_revenue += revenue[r]
+        if revenue[r] >= greatinc:
+            greatinc = revenue[r]
+            greatincmonth = months[r]
+        elif revenue[r] <= greatdec:
+            greatdec = revenue[r]
+            greatdecmonth = months[r]
+        totalrev = totalrev + revenue[r]
 
-#calculate average_change
-    average_change = round(total_revenue/total_months, 2)
+#calculate average change
+    avgchange = round(totalrev/totalmonths, 2)
 
 #create output file
     output_dest = os.path.join('Output','pybank_output_' + str(filenumber) + '.txt')
@@ -48,9 +48,9 @@ for filenumber in file_num:
 #print summary to output file
     with open(output_dest, 'w') as writefile:
         writefile.writelines('Financial Analysis\n')
-        writefile.writelines('----------------------------' + '\n')
-        writefile.writelines('Total Months: ' + str(total_months) + '\n')
-        writefile.writelines('Total Revenue: $' + str(total_revenue) + '\n')
-        writefile.writelines('Average Revenue Change: $' + str(average_change) + '\n')
-        writefile.writelines('Greatest Increase in Revenue: ' + great_inc_month + ' $' + str(greatest_inc) + '\n')
-        writefile.writelines('Greatest Decrease in Revenue: ' + great_dec_month + ' ($' + str(greatest_dec) + ')')
+        writefile.writelines('----------------------------------------------------------------' + '\n')
+        writefile.writelines('Total Months: ' + str(totalmonths) + '\n')
+        writefile.writelines('Total Revenue: $' + str(totalrev) + '\n')
+        writefile.writelines('Average Revenue Change: $' + str(avgchange) + '\n')
+        writefile.writelines('Greatest Increase in Revenue: ' + greatincmonth + ' $' + str(greatinc) + '\n')
+        writefile.writelines('Greatest Decrease in Revenue: ' + greatdecmonth + ' $' + str(greatdec) + '\n')
